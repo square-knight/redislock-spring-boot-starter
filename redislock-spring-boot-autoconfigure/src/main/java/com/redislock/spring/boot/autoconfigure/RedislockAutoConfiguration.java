d09ebd1ac2b5f3ccffe952cea0219ff54e1fcb47
fuxinpeng <xinpeng.fu@shoufuyou.com>
2018-10-23 11:20:51 +0800

    pom

--- redislock-spring-boot-autoconfigure/src/main/java/com/redislock/spring/boot/autoconfigure/RedislockAutoConfiguration.java
+++ redislock-spring-boot-autoconfigure/src/main/java/com/redislock/spring/boot/autoconfigure/RedislockAutoConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
